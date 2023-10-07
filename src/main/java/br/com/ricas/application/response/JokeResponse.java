5e100eef2b76edf17ed1cd095c8bdc1c3eb1e0a2
Ricardo <ricardohsmello@gmail.com>
2023-10-07 19:48:18 -0300

    refactor

--- src/main/java/br/com/ricas/application/response/JokeResponse.java
+++ src/main/java/br/com/ricas/application/response/JokeResponse.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class JokeResponse {
     private String value;
-
     public String getValue() {
         return value;
     }
